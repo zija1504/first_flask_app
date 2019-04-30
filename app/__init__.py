343830fb6f30d0aaa9321633be653ddf5be38bb5
zija1504 <zija1504@gmail.com>
2019-04-30 20:26:10 +0200

    login form

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
-app = Flask(__name__)
-from app import routes
+from config import Config
 
+app = Flask(__name__)
+app.config.from_object(Config)
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,39 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Jacek'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+    posts = [{
+        'author': {
+            'username': 'John'
         },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+        'body': 'Beautiful day in Portland!'
+    }, {
+        'author': {
+            'username': 'Susan'
+        },
+        'body': 'The Avengers movie was so cool!'
+    }]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" %
+                  (getattr(form, field).label.text, error))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # form.validate()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
